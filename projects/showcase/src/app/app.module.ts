3e246c3aad0b2a65bee4530cb2836ed0abc66f3b
10cent <hung10t3@gmail.com>
2023-02-09 10:58:50 +0700

    refactor showcase

--- projects/showcase/src/app/app.module.ts
+++ projects/showcase/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ButtonModule } from '@va/ui-sdk/button';
+import { UiSdkModule } from '@va/ui-sdk';
+import { CardComponent } from '@va/ui-sdk/card';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UiSdkModule,
+    ButtonModule,
+    CardComponent,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
