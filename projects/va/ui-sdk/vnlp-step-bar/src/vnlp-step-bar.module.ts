ec74863fa47eb9120aab2cd14cb5c685479fb3bf
truongnguyen1582000 <truongnguyen1582000>
2023-03-13 15:06:38 +0700

    remove comment

--- projects/va/ui-sdk/vnlp-step-bar/src/vnlp-step-bar.module.ts
+++ projects/va/ui-sdk/vnlp-step-bar/src/vnlp-step-bar.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VnlpStepBarComponent } from './vnlp-step-bar.component';
-// import { I18nModule } from '@em-and-ai/ui-sdk/i18n';
 
 @NgModule({
   declarations: [VnlpStepBarComponent],
