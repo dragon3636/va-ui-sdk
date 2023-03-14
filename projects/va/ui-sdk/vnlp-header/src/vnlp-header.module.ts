4697c34d3eaf8d654f1b91551d160200eb241b48
Cong Tran <tranvanthanhcooong@gmail.com>
2023-03-14 08:32:10 +0700

    update code

--- projects/va/ui-sdk/vnlp-header/src/vnlp-header.module.ts
+++ projects/va/ui-sdk/vnlp-header/src/vnlp-header.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { VnlpHeaderComponent } from './vnlp-header.component';
-import { VnlpAvatarModule } from '@em-and-ai/ui-sdk/vnlp-avatar';
 import { VnlpSearchModule } from '@em-and-ai/ui-sdk/vnlp-search';
+import { VnlpAvatarModule } from '@em-and-ai/ui-sdk/vnlp-avatar';
 import { VnlpBreadcrumbModule } from '@em-and-ai/ui-sdk/vnlp-breadcrumb';
+
 @NgModule({
   declarations: [VnlpHeaderComponent],
   imports: [
